0374f7eed2456b3372bc28b4220c7df8876acf40
Brice Kutuk <bricekutuk@live.fr>
2026-03-24 14:55:22 +0100

    try of props

--- principal/src/App.tsx
+++ principal/src/App.tsx
@@ -1,10 +1,21 @@
-import './App.css'
+import "./App.css";
 import PokemonCard from "./components/pokemonCard";
+
+const pokemonList = [
+  {
+    name: "bulbasaur",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+  },
+  {
+    name: "mew",
+  },
+];
 
 function App() {
   return (
-    <div> 
-      <PokemonCard/>
+    <div>
+      <PokemonCard pokemon={pokemonList[0]} />
     </div>
   );
 }
